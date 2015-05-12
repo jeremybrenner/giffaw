19a001e3b8adc0d395a4a2cf25ff54862a64707e
jmbrenne <jeremybrenner0@gmail.com>
2015-05-11 22:25:52 -0700

    Set default page to trending

--- base.js
+++ base.js
@@ -1,14 +1,22 @@
 $(document).ready(function() {
 
+	$.get("http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC", render)
+
+
     $('input').on('change', function() {
-        var $input = $('input').val()
+        var $input = $('input').val().replace("", "+")//is not necessary per API - punctuation is stripped.
         $.get("http://api.giphy.com/v1/gifs/search?q=" + $input + "&api_key=dc6zaTOxFJmzC", render)
     })
 
+    $('body').on('scroll', function(){
+    	var $input = $('input').val().replace("", "+")
+    	$.get("http://api.giphy.com/v1/gifs/search?q=" + $input + "&api_key=dc6zaTOxFJmzC", render)
+    })
+
     function render(data) {
+    	$("img").remove();
         $.each(data.data, function() {
             $("body").append("<img src='" + this.images.fixed_height.url + "'>");
         })
-
     }
-});
+}); 
