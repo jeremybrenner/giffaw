ea2535772f2c6b9c2cb73792c37222c8038b437a
jmbrenne <jeremybrenner0@gmail.com>
2015-05-11 18:36:06 -0700

    Added input response rendering

--- base.js
+++ base.js
@@ -1,4 +1,14 @@
-$(document).ready(function(){
-  // code in here!
-  console.log("here's your data:", mockData)
-})
+$(document).ready(function() {
+
+    $('input').on('change', function() {
+        var $input = $('input').val()
+        $.get("http://api.giphy.com/v1/gifs/search?q=" + $input + "&api_key=dc6zaTOxFJmzC", render)
+    })
+
+    function render(data) {
+        $.each(data.data, function() {
+            $("body").append("<img src='" + this.images.fixed_height.url + "'>");
+        })
+
+    }
+});
